51a59fecbf51bb7d516032b127b480082f4bad77
丁周杰 <dingzj@163.com>
2022-06-02 11:33:56 +0800

    master add again

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("新建");
         System.out.println("第一次修改");
         System.out.println("dev add");
+        System.out.println("master add");
     }
 }
